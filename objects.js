fbfa35d954e9706c414bc0231a09b39464b6167f
Melissa Heying <melissakheying@gmail.com>
2020-05-15 17:33:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,7 @@
-var playlist = {0}
+var playlist = {
+  'Marine Boudeau': 'Something',
+    'Someone else': 'Something else'
+}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
